17c1de0f0b0c01c781dce9799bb2edc28bb730c4
Besa <bjacaj19@epoka.edu.al>
2022-02-08 21:51:32 +0100

    Added php files

---
+++ common/header.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+        <title>Hello, world!</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Url Shortener</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Links -->
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link 3</a>
+                    </li>
+                </ul>
+
+                <span class="navbar-text">
+                    <a class="btn btn-primary btn-sm navbar-button" href="#" role="button">Login</a>
+                </span>
+            </div>
+        </nav>
+
+    
--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php echo 'Hello not world'; ?>
+<?php
+include_once "common/header.php";
+?>
+
+<div>
+    <h1>Shorten your URL!</h1>
+    <input type="text"/>
+    <button>Shorten!</button>
+</div>
+<?php
+include_once "common/footer.php";
+?>
